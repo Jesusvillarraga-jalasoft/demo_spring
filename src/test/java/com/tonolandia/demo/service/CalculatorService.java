947a4e403758b2c5524220fcc2459b09bdfaa118
Jesus.Villarraga <jesusantoniovillarraga@gmail.com>
2025-09-12 18:05:36 -0500

    Demo_Spring - Naves - dañemos esa monda

--- src/test/java/com/tonolandia/demo/service/CalculatorService.java
+++ src/test/java/com/tonolandia/demo/service/CalculatorService.java
@@ -14,13 +14,4 @@ class CalculatorServiceTest {
         assertEquals(5, calculator.add(2, 3));
     }
 
-    @Test
-    void testDivide() {
-        assertEquals(2, calculator.divide(6, 3));
-    }
-
-    @Test
-    void testDivideByZero() {
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
-    }
 }
